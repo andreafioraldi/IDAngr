3559413663fb12127bcfb8090accc6a22b16ecb1
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-02-06 21:50:02 +0100

    added StateShot

--- idangr.py
+++ idangr.py
@@ -2,28 +2,40 @@ from memory import SimSymbolicIdaMemory
 import angr
 import idaapi
 import idc
+import claripy
 
+project = None
 
-idc.RefreshDebuggerMemory()
+def StateShot():
+    global project
+    
+    idc.RefreshDebuggerMemory()
 
-fpath = idaapi.get_input_file_path()
+    fpath = idaapi.get_input_file_path()
 
-project = angr.Project(fpath, load_options={"auto_load_libs":False})
+    if project == None:
+        project = angr.Project(fpath, load_options={"auto_load_libs":False})
 
-mem = SimSymbolicIdaMemory(memory_backer=project.loader.memory, permissions_backer=None, memory_id="mem")
+    mem = SimSymbolicIdaMemory(memory_backer=project.loader.memory, permissions_backer=None, memory_id="mem")
 
-state = project.factory.blank_state(plugins={"memory": mem})
+    state = project.factory.blank_state(plugins={"memory": mem})
 
-b = project.arch.bits / 8
+    # = project.arch.bits / 8
 
-for reg in sorted(project.arch.registers, key=lambda x: project.arch.registers.get(x)[1]):
-    if reg in ("sp", "bp", "ip"):
-        continue
-    try:
-        setattr(state.regs, reg, idc.GetRegValue(reg))
-        #print reg, hex(idc.GetRegValue(reg))
-    except:
-        #print "fail to set register", reg
-        pass
+    for reg in sorted(project.arch.registers, key=lambda x: project.arch.registers.get(x)[1]):
+        if reg in ("sp", "bp", "ip"):
+            continue
+        try:
+            setattr(state.regs, reg, idc.GetRegValue(reg))
+            #print reg, hex(idc.GetRegValue(reg))
+        except:
+            #print "fail to set register", reg
+            pass
+    
+    return state
 
-
+print
+print "########### IDAngr ###########"
+print "  usage: state = StateShot()"
+print "##############################"
+print
