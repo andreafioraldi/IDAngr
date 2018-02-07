23dc0ee2da52d998e2af8feabfd6bbf3afe8fd1b
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-02-07 15:27:27 +0100

    added StateManager

--- README.md
+++ README.md
@@ -5,9 +5,43 @@ use angr in the ida debugger taking as state the current debugger state
 
 ### usage
 
-1. Load the idangr.py script from the ida menu to create an angr project.
-2. During the debug type on the shell 'state = StateShot()' to create an angr state from the current debugger state.
+1. Load the idangr.py script from the ida menu to create an angr project
+2. During the debug get an instance of StateManager or StateShot
 3. Do things with angr
 4. Return to 2 or exit
 
+### api
 
+#### StateShot
+
+Return an angr state from the current debug session state.
+
+#### StateManager
+
+A wrapper around angr to simplify the symbolic values creation and to write the result back on the debugger when angr found a valid path.
+
+##### methods
++ `instance.sim(key, size)`        create a symbolic value on a register or on a memory address (size is optional)
++ `instance[key]`                  get a symbolic value created previously
++ `instance.simulation_manager()`  create an angr simulation manager based on the state
++ `instance.to_dbg(found_state)`   transfer to the debugger state the evaluated value of the symbolic value created before with sim
+
+### example
+
+```python
+Python>sm = StateManager()
+Python>sm.sim("edi")
+Python>sm.sim("esi")
+Python>m = sm.simulation_manager()
+Python>m.explore(avoid=0x04005D5, find=0x00004005BC)
+<SimulationManager with 1 found, 3 avoid>
+Python>idc.GetRegValue("edi")
+0
+Python>idc.GetRegValue("esi")
+5
+Python>sm.to_dbg(m.found[0])
+Python>idc.GetRegValue("edi")
+2
+Python>idc.GetRegValue("esi")
+0
+```
--- idangr.py
+++ idangr.py
@@ -6,6 +6,8 @@ import claripy
 
 project = None
 
+__IDANGR_BITS = None
+
 def StateShot():
     global project
     
@@ -15,12 +17,11 @@ def StateShot():
 
     if project == None:
         project = angr.Project(fpath, load_options={"auto_load_libs":False})
+        __IDANGR_BITS = project.arch.bits
 
     mem = SimSymbolicIdaMemory(memory_backer=project.loader.memory, permissions_backer=None, memory_id="mem")
 
     state = project.factory.blank_state(plugins={"memory": mem})
-
-    # = project.arch.bits / 8
 
     for reg in sorted(project.arch.registers, key=lambda x: project.arch.registers.get(x)[1]):
         if reg in ("sp", "bp", "ip"):
@@ -34,8 +35,63 @@ def StateShot():
     
     return state
 
+
+
+class StateManager(object):
+    def __init__(self):
+        self.state = StateShot()
+        self.symbolics = {}
+    
+    def sim(self, key, size=None):
+        '''
+        key: memory address(int) or register name(str)
+        size: size of object in bytes
+        '''
+        if key in project.arch.registers:
+            if size == None:
+                size = project.arch.registers[key][1]
+            size *= 8
+            s = claripy.BVS("idangr_reg_" + str(key), size)
+            setattr(self.state.regs, key, s)
+            self.symbolics[key] = (s, size)
+        elif type(key) == int:
+            if size == None:
+                size = __IDANGR_BITS
+            else:
+                size *= 8
+            s = claripy.BVS("idangr_mem_" + hex(key), size)
+            self.state.memory.store(key, s)
+            self.symbolics[key] = (s, size)
+        else:
+            raise ValueError("key must be a register name or a memory address")
+    
+    def __getitem__(self, key):
+        if key not in self.symbolics:
+            return None
+        return self.symbolics[key][0]
+    
+    def simulation_manager(self):
+        return project.factory.simulation_manager(self.state)
+    
+    def to_dbg(self, found_state):
+        for key in self.symbolics:
+            try:
+                if key in project.arch.registers:
+                    r = found_state.solver.eval(self.symbolics[key][0], cast_to=int)
+                    idc.SetRegValue(r, key)
+                else:
+                    r = found_state.solver.eval(self.symbolics[key][0], cast_to=str)
+                    for i in xrange(len(r)):
+                        idc.PatchByte(key + i, r[i])
+            except Exception as ee:
+                print " >> failed to write %s to debugger" % key
+                #print ee
+
+
 print
 print "########### IDAngr ###########"
-print "  usage: state = StateShot()"
+print "  usage: sm = StateManager()"
 print "##############################"
 print
+
+
