019b143e64aaa848d4a226a7ae39f7bff75bc39b
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-02-08 18:55:56 +0100

    added SymbolicsSet

--- idangr.py
+++ idangr.py
@@ -33,6 +33,50 @@ def StateShot():
     return state
 
 
+class SimbolicsSet(object):
+    def __init__(self):
+        self.symbolics = {}
+    
+    def add(self, key, size=None):
+        '''
+        key: memory address(int) or register name(str)
+        size: size of object in bytes
+        '''
+        if key in project.arch.registers:
+            if size == None:
+                size = project.arch.registers[key][1]
+            size *= 8
+            s = claripy.BVS("idangr_reg_" + str(key), size)
+            self.symbolics[key] = (s, size)
+        elif type(key) == int or type(key) == long:
+            if size == None:
+                size = project.arch.bits
+            else:
+                size *= 8
+            s = claripy.BVS("idangr_mem_" + hex(key), size)
+            self.symbolics[key] = (s, size)
+        elif type(key) == claripy.ast.bv.BV:
+            key = self.state.solver.eval(key, cast_to=int)
+            self.sim(key, size)
+        else:
+            raise ValueError("key must be a register name or a memory address, not %s" % str(type(key)))
+    
+    def remove(self, key):
+        if type(key) == claripy.ast.bv.BV:
+            key = self.state.solver.eval(key, cast_to=int)
+        del self.symbolics[key]
+    
+    def regs(self):
+        for key in self.symbolics:
+            if type(key) == str:
+                yield key
+    
+    def mems(self):
+        for key in self.symbolics:
+            if type(key) != str:
+                yield (key, self.symbolics[key][1])
+    
+
 class StateManager(object):
     def __init__(self, state=None):
         self.state = StateShot() if state is None else state
@@ -63,6 +107,13 @@ class StateManager(object):
             self.sim(key, size)
         else:
             raise ValueError("key must be a register name or a memory address, not %s" % str(type(key)))
+    
+    def sim_from_set(self, simset):
+        for key in simset.symbolics:
+            if key in project.arch.registers:
+                setattr(self.state.regs, key, simset.symbolics[key][0])
+            else:
+                self.state.memory.store(key, simset.symbolics[key][0])
     
     def __getitem__(self, key):
         if key in project.arch.registers:
