e9556274379bc4ea2eb6105b8f2367862f74bb93
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-12-09 14:37:19 +0100

    PIN dbg class

--- idangr/ida_debugger.py
+++ idangr/ida_debugger.py
@@ -36,13 +36,14 @@ SEG_PROT_R = 4
 SEG_PROT_W = 2
 SEG_PROT_X = 1
 
-class IdaRemoteDebugger(object):
+class IdaDebugger(object):
     #must implements all methods of angrdbg.Debugger !!!
     
-    def __init__(self, angrdbg_mod):
+    def __init__(self, angrdbg_mod, remote=False):
         #self.get_byte = idaapi.get_byte
         self.name = "IDAngr"
         self.angrdbg_mod = angrdbg_mod
+        self.remote = remote
     
     #-------------------------------------
     def before_stateshot(self):
@@ -58,8 +59,10 @@ class IdaRemoteDebugger(object):
     #-------------------------------------
     def input_file(self):
         path = idaapi.get_input_file_path()
-        return RemoteFile(open(path, "rb"), path)
-        #return path
+        f = open(path, "rb")
+        if self.remote:
+            return RemoteFile(f, path)
+        return f
     
     def image_base(self):
         return idaapi.get_imagebase()
@@ -154,6 +157,12 @@ class IdaRemoteDebugger(object):
     
     def get_idata(self): #return tuple(start_addr, end_addr)
         ida_seg = idaapi.get_segm_by_name(".idata")
+        if ida_seg is None:
+            addr = None
+            def cb(ea, name, i):
+                addr = ea
+            idaapi.enum_import_names(0, cb)
+            ida_seg = idaapi.seg_by_addr(addr)
         return (ida_seg.start_ea, ida_seg.end_ea)
     
     #-------------------------------------
@@ -164,136 +173,35 @@ class IdaRemoteDebugger(object):
             return None
 
 
-
-class IdaLocalDebugger(object):
-    #must implements all methods of angrdbg.Debugger !!!
+class IdaPinDebugger(IdaDebugger):
     
-    def __init__(self, angrdbg_mod):
-        #self.get_byte = idaapi.get_byte
-        self.name = "IDAngr"
-        self.angrdbg_mod = angrdbg_mod
-        
-        self.get_byte = idaapi.get_byte
-    
-    #-------------------------------------
     def before_stateshot(self):
-        pass
-        
-    def after_stateshot(self, state):
-        pass
-
-    #-------------------------------------
-    def is_active(self):
-        return idaapi.is_debugger_on() and idaapi.dbg_can_query()
-    
-    #-------------------------------------
-    def input_file(self):
-        path = idaapi.get_input_file_path()
-        return open(path, "rb")
-    
-    def image_base(self):
-        return idaapi.get_imagebase()
-    
-    #-------------------------------------
-    #def get_byte(self, addr):
-    #    return idaapi.get_byte(addr)
-    
-    def get_word(self, addr):
-        return idaapi.get_word(addr)
-    
-    def get_dword(self, addr):
-        return idaapi.get_dword(addr)
-    
-    def get_qword(self, addr):
-        return idaapi.get_qword(addr)
-    
-    def get_bytes(self, addr, size):
-        return idc.get_bytes(addr, size)
-    
-    def put_byte(self, addr, value):
-        idc.patch_dbg_byte(addr, value)
-    
-    def put_word(self, addr, value):
-        idc.patch_dbg_word(addr, value)
-    
-    def put_dword(self, addr, value):
-        idc.patch_dbg_dword(addr, value)
-    
-    def put_qword(self, addr, value):
-        idc.patch_dbg_qword(addr, value)
-    
-    def put_bytes(self, addr, value):
-        for i in xrange(len(value)):
-            idc.patch_dbg_byte(addr +i, ord(value[i]))
-    
-    #-------------------------------------
-    def get_reg(self, name):
-        return idc.get_reg_value(name)
-    
-    def set_reg(self, name, value):
-        idc.set_reg_value(value, name)
-    
-    #-------------------------------------
-    def step_into(self):
-        idaapi.step_into()
-    
-    def run(self):
-        pass
-    
-    def wait_ready(self):
-        idc.GetDebuggerEvent(idc.WFNE_SUSP, -1)
-    
-    def refresh_memory(self):
-        idc.refresh_debugger_memory()
-    
-    #-------------------------------------
-    def seg_by_name(self, name):
-        ida_seg = idaapi.get_segm_by_name(name)
-        if ida_seg is None:
-            return None
-        perms = 0
-        perms |= SEG_PROT_R if ida_seg.perm & idaapi.SEGPERM_READ else 0
-        perms |= SEG_PROT_W if ida_seg.perm & idaapi.SEGPERM_WRITE else 0
-        perms |= SEG_PROT_X if ida_seg.perm & idaapi.SEGPERM_EXEC else 0
-        return self.angrdbg_mod.Segment(name, ida_seg.start_ea, ida_seg.end_ea, perms)
+        import win_vmmap
+        pid = send_dbg_command("getpid")
+        self.vmmap = win_vmmap.vmmap(pid)
     
     def seg_by_addr(self, addr):
         ida_seg = idaapi.getseg(addr)
-        if ida_seg is None:
-            return None
+        name = "<no name>"
+        if ida_seg is not None:
+            name = ida_seg.name
+        for start, end, perms, name in self.vmmap:
+            if addr >= start and addr < end:
+                return Segment(name, start, end, perms)
+        # fallback on ida segs
         perms = 0
         perms |= SEG_PROT_R if ida_seg.perm & idaapi.SEGPERM_READ else 0
         perms |= SEG_PROT_W if ida_seg.perm & idaapi.SEGPERM_WRITE else 0
         perms |= SEG_PROT_X if ida_seg.perm & idaapi.SEGPERM_EXEC else 0
         return self.angrdbg_mod.Segment(ida_seg.name, ida_seg.start_ea, ida_seg.end_ea, perms)
 
-    def get_got(self): #return tuple(start_addr, end_addr)
-        ida_seg = idaapi.get_segm_by_name(self.angrdbg_mod.load_project().arch.got_section_name)
-        return (ida_seg.start_ea, ida_seg.end_ea)
-    
-    def get_plt(self): #return tuple(start_addr, end_addr)
-        ida_seg = idaapi.get_segm_by_name(".plt")
-        return (ida_seg.start_ea, ida_seg.end_ea)
-    
-    def get_idata(self): #return tuple(start_addr, end_addr)
-        ida_seg = idaapi.get_segm_by_name(".idata")
-        return (ida_seg.start_ea, ida_seg.end_ea)
-    
-    #-------------------------------------
-    def resolve_name(self, name): #return None on fail
-        try:
-            return idaapi.get_debug_name_ea(name)
-        except:
-            return None
-
-
 
 def register(conn):
     if conn:
-        conn[0].modules.angrdbg.register_debugger(IdaRemoteDebugger(conn[0].modules.angrdbg))
-        conn[1].modules.angrdbg.register_debugger(IdaRemoteDebugger(conn[1].modules.angrdbg))
+        conn[0].modules.angrdbg.register_debugger(IdaDebugger(conn[0].modules.angrdbg, True))
+        conn[1].modules.angrdbg.register_debugger(IdaDebugger(conn[1].modules.angrdbg, True))
     else:
-        get_angrdbg().register_debugger(IdaLocalDebugger(get_angrdbg()))
+        get_angrdbg().register_debugger(IdaDebugger(get_angrdbg(), False))
     
 
 
---
+++ idangr/win_vmmap.py
@@ -0,0 +1,69 @@
+import win32con
+import win32api
+
+import ctypes
+from ctypes import windll, wintypes
+
+SEG_PROT_R = 4
+SEG_PROT_W = 2
+SEG_PROT_X = 1
+
+
+class MEMORY_BASIC_INFORMATION_64(ctypes.Structure):
+    _fields_ = [
+        ("BaseAddress", wintypes.LARGE_INTEGER),
+        ("AllocationBase", wintypes.LARGE_INTEGER),
+        ("AllocationProtect", wintypes.DWORD),
+        ("RegionSize", wintypes.LARGE_INTEGER),
+        ("State", wintypes.DWORD),
+        ("Protect", wintypes.DWORD),
+        ("Type", wintypes.DWORD)
+    ]
+
+class MEMORY_BASIC_INFORMATION_32(ctypes.Structure):
+    _fields_ = [
+        ("BaseAddress", wintypes.DWORD),
+        ("AllocationBase", wintypes.DWORD),
+        ("AllocationProtect", wintypes.DWORD),
+        ("RegionSize", wintypes.UINT),
+        ("State", wintypes.DWORD),
+        ("Protect", wintypes.DWORD),
+        ("Type", wintypes.DWORD)
+    ]
+
+def vmmap(pid, is_64=True):
+    base = 0
+    if is_64:
+        mbi = MEMORY_BASIC_INFORMATION_64()
+        addr_type = wintypes.LARGE_INTEGER
+    else:
+        mbi = MEMORY_BASIC_INFORMATION_32()
+        addr_type = wintypes.DWORD
+    proc = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION, 0, pid)
+    
+    maps = []
+    while windll.kernel32.VirtualQueryEx(proc.handle, addr_type(base), ctypes.byref(mbi), ctypes.sizeof(mbi)) > 0:
+        mapperm = 0
+        if mbi.Protect & win32con.PAGE_EXECUTE:
+            mapperm = SEG_PROT_X
+        elif mbi.Protect & win32con.PAGE_EXECUTE_READ:
+            mapperm = SEG_PROT_X | SEG_PROT_R
+        elif mbi.Protect & win32con.PAGE_EXECUTE_READWRITE:
+            mapperm = SEG_PROT_X | SEG_PROT_R | SEG_PROT_W
+        elif mbi.Protect & win32con.PAGE_EXECUTE_WRITECOPY:
+            mapperm = SEG_PROT_X | SEG_PROT_R
+        elif mbi.Protect & win32con.PAGE_NOACCESS:
+            mapperm = 0
+        elif mbi.Protect & win32con.PAGE_READONLY:
+            mapperm = SEG_PROT_R
+        elif mbi.Protect & win32con.PAGE_READWRITE:
+            mapperm = SEG_PROT_R | SEG_PROT_W
+        elif mbi.Protect & win32con.PAGE_WRITECOPY:
+            mapperm = SEG_PROT_R
+        print hex(mbi.BaseAddress) +"\t"+ hex(mbi.BaseAddress + mbi.RegionSize) +"\t"+ hex(mapperm)
+        maps.append((mbi.BaseAddress, mbi.BaseAddress + mbi.RegionSize, mapperm, ""))
+        base += mbi.RegionSize
+        
+    win32api.CloseHandle(proc)
+    return maps
+
